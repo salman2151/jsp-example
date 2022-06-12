05c2d7cf99a1792a977c50d8f1238f7cb7cd9432
salmann2151 <salmannisar2151@gmail.com>
2022-06-12 17:09:14 +0500

    sql updated

--- db_script/script.sql
+++ db_script/script.sql
@@ -26,14 +26,32 @@ category_id_fk int NOT NULL ,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
+
 INSERT INTO blog (`category_id_fk`, `title`,`description`) VALUES (17, "TitleOp","DescriptionOP");
+CREATE TABLE category (
+id int NOT NULL AUTO_INCREMENT,
+ name varchar(200) NOT NULL,
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 select * from category;
-select * from blog;
-
-
-
-
-SELECT blog.id, blog.title,blog.description,blog.created_on, category.id as category_id, category.name as category_name FROM blog,category WHERE category.id=blog.category_id_fk and blog.id = 7
+	
+SELECT blog.id, blog.title,blog.description,blog.created_on, category.id as category_id, category.name as category_name FROM blog,category WHERE category.id=blog.category_id_fk and blog.id = 7;
 
 SELECT * FROM (SELECT blog.id, blog.title,blog.description,blog.created_on, category.id as category_id, category.name as category_name FROM blog,category WHERE category.id=blog.category_id_fk ORDER BY blog.id DESC LIMIT 3) t ORDER BY id DESC;
+
+
+CREATE TABLE comment (
+id int NOT NULL AUTO_INCREMENT,
+blog_id_fk int NOT NULL ,
+commenter varchar(200) NOT NULL,
+  comment varchar(10000) DEFAULT NULL,
+  created_on TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id),
+  FOREIGN KEY (blog_id_fk)
+      REFERENCES blog( id)
+      ON UPDATE CASCADE ON DELETE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
